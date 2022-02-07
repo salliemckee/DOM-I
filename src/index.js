2686a5b7dec2f5ef6ce7d1f0e383a4e0756383b1
Sallie McKee <salliemckee@Morrigan.local>
2022-02-07 16:40:42 -0600

    added nav, img, footer, contact, and top content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const siteContent = { // DO NOT CHANGE THIS OBJECT
-  "nav": {
+const siteContent = {
+  // DO NOT CHANGE THIS OBJECT
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -7,36 +8,109 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "nav-item-5": "About",
     "nav-item-6": "Contact",
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
   },
   "main-content": {
     "features-h4": "Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4": "About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "services-h4": "Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "product-h4": "Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vision-h4": "Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "contact": {
+  contact: {
     "contact-h4": "Contact",
-    "address": "123 Way 456 Street Somewhere, USA",
-    "phone": "1 (888) 888-8888",
-    "email": "sales@greatidea.io",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io",
   },
-  "footer": {
-    "copyright": "Copyright Great Idea! 2021",
+  footer: {
+    copyright: "Copyright Great Idea! 2021",
   },
-  "images": {
+  images: {
     "logo-img": "http://localhost:9000/img/logo.png",
     "cta-img": "http://localhost:9000/img/cta.png",
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
 
-console.log('project wired!')
+console.log("project wired!");
+
+//nav content//
+
+const serviceNav = document.querySelector("nav a:nth-of-type(1)");
+const productNav = document.querySelector("nav a:nth-of-type(2)");
+const visionNav = document.querySelector("nav a:nth-of-type(3)");
+const featuresNav = document.querySelector("nav a:nth-of-type(4)");
+const aboutNav = document.querySelector("nav a:nth-of-type(5)");
+const contactNav = document.querySelector("nav a:nth-of-type(6)");
+
+serviceNav.textContent = siteContent["nav"]["nav-item-1"];
+productNav.textContent = siteContent["nav"]["nav-item-2"];
+visionNav.textContent = siteContent["nav"]["nav-item-3"];
+featuresNav.textContent = siteContent["nav"]["nav-item-4"];
+aboutNav.textContent = siteContent["nav"]["nav-item-5"];
+contactNav.textContent = siteContent["nav"]["nav-item-6"];
+
+//img content//
+const logoImg = document.querySelector("#logo-img");
+logoImg.src = siteContent.images["logo-img"];
+
+const ctaImg = document.querySelector("#cta-img");
+ctaImg.src = siteContent.images["cta-img"];
+
+const midImg = document.querySelector("#middle-img");
+midImg.src = siteContent.images["accent-img"];
+
+//footer content//
+const footerLink = document.querySelector("footer a");
+footerLink.textContent = siteContent.footer.copyright;
+footerLink.classList.add("bold");
+
+//contact content//
+const contact = document.querySelector("section.contact");
+contact.children[0].textContent = siteContent.contact["contact-h4"];
+contact.children[1].textContent = siteContent.contact["address"];
+contact.children[2].textContent = siteContent.contact["phone"];
+contact.children[3].textContent = siteContent.contact["email"];
+
+//top content//
+const topContent = document.querySelector(".top-content");
+topContent.children[0].children[0].textContent =
+  siteContent["main-content"]["features-h4"];
+topContent.children[0].children[1].textContent =
+  siteContent["main-content"]["features-content"];
+
+topContent.children[1].children[0].textContent =
+  siteContent["main-content"]["about-h4"];
+topContent.children[1].children[1].textContent =
+  siteContent["main-content"]["features-content"];
+
+//bottom content//
+const bottomContent = document.querySelector(".bottom-content");
+
+bottomContent.children[0].children[0].textContent =
+  siteContent["main-content"]["services-h4"];
+bottomContent.children[0].children[1].textContent =
+  siteContent["main-content"]["services-content"];
+
+bottomContent.children[1].children[0].textContent =
+  siteContent["main-content"]["product-h4"];
+bottomContent.children[1].children[1].textContent =
+  siteContent["main-content"]["product-content"];
+
+bottomContent.children[2].children[0].textContent =
+  siteContent["main-content"]["vision-h4"];
+bottomContent.children[2].children[1].textContent =
+  siteContent["main-content"]["vision-content"];
